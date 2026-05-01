87f7e4c975d009ac5d69de40ac03a12c79582911
DazzlerAbhi <ak3774041@gmail.com>
2026-05-01 12:37:57 +0530

    chore: lock down CORS policy to Vercel domain

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: [
+    'https://hackathon-website-eight-ebon.vercel.app',
+    'http://localhost:5173'
+  ],
   credentials: true
 }));
 app.use(express.json());
